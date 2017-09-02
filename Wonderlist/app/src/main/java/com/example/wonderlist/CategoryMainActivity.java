218fc7e5f97ee842cce1c6eeaeb8c8b37608f1d0
FalakD <30274347+FalakD@users.noreply.github.com>
2017-09-02 12:10:47 -0700

    category layout

---
+++ Wonderlist/app/src/main/java/com/example/wonderlist/CategoryMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wonderlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_main);
+    }
+}
