bf986d25c287b2a8c11d2259ea7273f0724b7c64
tejaskb04 <tejaskb@gmail.com>
2017-09-03 15:25:29 -0700

    Settings Code

--- Wonderlist/app/src/main/java/com/example/wonderlist/SettingsActivity.java
+++ Wonderlist/app/src/main/java/com/example/wonderlist/SettingsActivity.java
@@ -2,23 +2,28 @@ package com.example.wonderlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.FrameLayout;
-import android.widget.ListView;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
-
-    private ListView listView;
-    List categories = new ArrayList();
+    private TextView profile;
+    private TextView account;
+    private TextView notifications;
+    private TextView permissionDataStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        listView = (ListView) findViewById(R.id.settings_list);
+        profile = (TextView) findViewById(R.id.profile);
+        account = (TextView) findViewById(R.id.account);
+        notifications = (TextView) findViewById(R.id.notifications);
+        permissionDataStorage = (TextView) findViewById(R.id.permissions_data_storage);
+        profile.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, 0, 0);
+        account.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_account_circle_black_24dp, 0,
+                0, 0);
+        notifications.setCompoundDrawablesWithIntrinsicBounds(R.drawable
+                .ic_notifications_black_24dp, 0, 0, 0);
+        permissionDataStorage.setCompoundDrawablesWithIntrinsicBounds(R.drawable
+                .ic_perm_data_setting_black_24dp, 0, 0, 0);
     }
 }
